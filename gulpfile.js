fc540b0c336c21be3015e4d7c36337af67ef24b3
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2018-07-19 15:26:03 +0300

    fixesgusdgulp

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('sass', function () {
 gulp.task('scripts', function () {
     gulp.src(source.js)
         .pipe(concat('scripts.min.js'))
-        // .pipe(uglify())
+        .pipe(uglify())
         .pipe(gulp.dest('dist/assets/js'))
         .pipe(livereload({ start: true }));
 });
